52f0a6c0641cf0cb52a5ac09f0e447b09d639b58
swansrp <56093273@qq.com>
2023-08-16 16:54:32 +0800

    修正import

--- kernel/src/main/java/com/bidr/kernel/mybatis/repository/inf/SqlDeleteRepo.java
+++ kernel/src/main/java/com/bidr/kernel/mybatis/repository/inf/SqlDeleteRepo.java
@@ -3,7 +3,6 @@ package com.bidr.kernel.mybatis.repository.inf;
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
 
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
