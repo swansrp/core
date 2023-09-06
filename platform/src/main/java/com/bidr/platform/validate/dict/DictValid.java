fc9f6be839533bfb2f71e74321104ffdeb37341a
swansrp <56093273@qq.com>
2023-09-06 10:42:23 +0800

    添加字典参数输入参数校验

---
+++ platform/src/main/java/com/bidr/platform/validate/dict/DictValid.java
@@ -0,0 +1,34 @@
+package com.bidr.platform.validate.dict;
+
+import com.bidr.kernel.constant.dict.Dict;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * Title: DictValid
+ * Description: Copyright: Copyright (c) 2023
+ *
+ * @author Sharp
+ * @since 2023/09/06 09:57
+ */
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {DictValidator.class})
+@Documented
+public @interface DictValid {
+
+    String message() default "输入的参数不合法";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * 枚举类
+     *
+     * @return 枚举类
+     */
+    Class<? extends Dict>[] target() default {};
+}
---
+++ platform/src/main/java/com/bidr/platform/validate/dict/DictValidator.java
@@ -0,0 +1,75 @@
+package com.bidr.platform.validate.dict;
+
+import com.bidr.kernel.constant.dict.Dict;
+import com.bidr.kernel.constant.dict.MetaDict;
+import com.bidr.kernel.utils.BeanUtil;
+import com.bidr.kernel.utils.FuncUtil;
+import com.bidr.platform.dao.entity.SysDict;
+import com.bidr.platform.service.cache.dict.DictCacheService;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+/**
+ * Title: DictValidator
+ * Description: Copyright: Copyright (c) 2023
+ *
+ * @author Sharp
+ * @since 2023/09/06 09:57
+ */
+public class DictValidator implements ConstraintValidator<DictValid, Object> {
+    /**
+     * 目标枚举类
+     */
+    Class<? extends Dict>[] dictClassArray;
+
+    @Override
+    public void initialize(DictValid constraintAnnotation) {
+        dictClassArray = constraintAnnotation.target();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        if (FuncUtil.isEmpty(dictClassArray)) {
+            return true;
+        } else if (FuncUtil.isEmpty(value)) {
+            return true;
+        } else if (value instanceof List) {
+            List<?> values = (List<?>) value;
+            for (Object val : values) {
+                if (!isValidForValue(val, dictClassArray)) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return isValidForValue(value, dictClassArray);
+        }
+    }
+
+    private boolean isValidForValue(Object value, Class<? extends Dict>[] cls) {
+        if (value != null && value.toString().length() > 0 && cls.length > 0) {
+            for (Class<? extends Dict> cl : cls) {
+                DictCacheService cacheService = BeanUtil.getBean(DictCacheService.class);
+                if (FuncUtil.isNotEmpty(cacheService)) {
+                    MetaDict metaDict = cl.getAnnotation(MetaDict.class);
+                    if (FuncUtil.isEmpty(metaDict)) {
+                        return true;
+                    } else {
+                        String dictName = metaDict.value();
+                        SysDict dictByValue = cacheService.getDictByValue(dictName, value.toString());
+                        return FuncUtil.isNotEmpty(dictByValue);
+                    }
+
+                } else {
+                    return true;
+                }
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+}
