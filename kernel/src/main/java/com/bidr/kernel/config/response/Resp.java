88b8e1f0d8af3b4bdcad5eb598e7d456162fc997
swansrp <56093273@qq.com>
2023-10-23 10:02:56 +0800

    添加一个转换方法 根据给定列表dict 将entity进行填充 并转换成目标列表

--- kernel/src/main/java/com/bidr/kernel/config/response/Resp.java
+++ kernel/src/main/java/com/bidr/kernel/config/response/Resp.java
@@ -2,11 +2,20 @@ package com.bidr.kernel.config.response;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.bidr.kernel.common.func.GetFunc;
 import com.bidr.kernel.exception.NoticeException;
+import com.bidr.kernel.utils.FuncUtil;
+import com.bidr.kernel.utils.JsonUtil;
+import com.bidr.kernel.utils.LambdaUtil;
+import com.bidr.kernel.utils.ReflectionUtil;
 import com.diboot.core.binding.Binder;
 import lombok.Data;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Title: R
@@ -21,12 +30,63 @@ public class Resp {
         throw new NoticeException(String.format(noticeFormat, parameter));
     }
 
+    /**
+     * 将制定源数据转换成目标
+     *
+     * @param entity  源数据
+     * @param voClass 目标数据类型
+     * @param <T>     源数据类型
+     * @param <VO>    目标数据类型
+     * @return 目标数据
+     */
     public static <T, VO> VO convert(T entity, Class<VO> voClass) {
         return Binder.convertAndBindRelations(entity, voClass);
     }
 
-    public static <T, VO> List<VO> convert(List<T> entity, Class<VO> voClass) {
-        return Binder.convertAndBindRelations(entity, voClass);
+    /**
+     * 根据给定列表dict 将entity进行填充 并转换成目标列表
+     *
+     * @param entityList 源数据列表
+     * @param voClass    目标数据类型
+     * @param dictList   给定列表
+     * @param dictField  给定列表匹配字段
+     * @param dataFiled  源数据匹配点断
+     * @param <T>        源数据类型
+     * @param <VO>       目标数据类型
+     * @param <E>        给定列表类型
+     * @return 目标数据列表
+     */
+    public static <T, VO, E> List<VO> convert(List<T> entityList, Class<VO> voClass, List<E> dictList,
+                                              GetFunc<E, ?> dictField, GetFunc<T, ?> dataFiled) {
+        Map<Object, T> map = new LinkedHashMap<>();
+        if (FuncUtil.isNotEmpty(entityList)) {
+            if (FuncUtil.isNotEmpty(dictList)) {
+                for (E dict : dictList) {
+                    T entity = (T) ReflectionUtil.newInstance(entityList.get(0).getClass());
+                    Field field = LambdaUtil.getFieldByGetFunc(dataFiled);
+                    Object dictValue = JsonUtil.readJson(dictField.apply(dict), field.getType());
+                    ReflectionUtil.setValue(field, entity, dictValue);
+                    map.put(dictValue, entity);
+                }
+            }
+            for (T entity : entityList) {
+                map.put(dataFiled.apply(entity), entity);
+            }
+        }
+        return Binder.convertAndBindRelations(new ArrayList<>(map.values()), voClass);
+    }
+
+    /**
+     * 将源数据列表换成目标列表
+     *
+     * @param entityList 源数据列表
+     * @param voClass    目标数据类型
+     * @param <T>        源数据类型
+     * @param <VO>       目标数据类型
+     * @return 目标数据列表
+     */
+    public static <T, VO> List<VO> convert(List<T> entityList, Class<VO> voClass) {
+        return Binder.convertAndBindRelations(entityList, voClass);
     }
 
     public static <T, R> Page<R> convert(IPage<T> page, Class<R> clazz) {
