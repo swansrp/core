7c9e9f178c536fc6dfbf685b60f2978aaccffc9f
swansrp <56093273@qq.com>
2024-06-11 10:03:00 +0800

    更新restTemplate现在可以不走系统代理单独配置

--- qichacha/src/main/java/com/bidr/qcc/service/QccServiceImpl.java
+++ qichacha/src/main/java/com/bidr/qcc/service/QccServiceImpl.java
@@ -19,12 +19,17 @@ import com.bidr.qcc.dto.name.NameSearchReq;
 import com.bidr.qcc.dto.name.NameSearchRes;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpHost;
+import org.apache.http.impl.client.HttpClients;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -43,9 +48,19 @@ public class QccServiceImpl implements QccService {
     @Value("${qichacha.app-secret}")
     private String appSecret;
 
+    @Value("${qichacha.proxy.enable}")
+    private boolean proxyEnable;
+    @Value("${qichacha.proxy.host}")
+    private String proxyHost;
+    @Value("${qichacha.proxy.port}")
+    private Integer proxyPort;
+
+    @Resource
+    private RestTemplate restTemplate;
+
     protected <T> T get(String url, QccReq req, Class<T> clazz) {
         HttpHeaders header = buildHttpHeaders(req);
-        QccRes<T> res = restService.get(url, header, req, QccRes.class, clazz);
+        QccRes<T> res = restService.get(getRestTemplate(), url, header, req, QccRes.class, clazz);
         handleError(res);
         return res.getResult();
     }
@@ -63,7 +78,7 @@ public class QccServiceImpl implements QccService {
 
     protected <T> Page<T> getPage(String url, QccReq req, Class<T> clazz) {
         HttpHeaders header = buildHttpHeaders(req);
-        QccRes<List<T>> res = restService.get(url, header, req, QccRes.class, List.class);
+        QccRes<List<T>> res = restService.get(getRestTemplate(), url, header, req, QccRes.class, List.class);
         handleError(res);
         Page<T> result = new Page<>(res.getPaging().getPageIndex(), res.getPaging().getPageSize(),
                 res.getPaging().getTotalRecords());
@@ -95,8 +110,7 @@ public class QccServiceImpl implements QccService {
     public EnterpriseRes getEnterpriseInfo(EnterpriseReq req) {
         String searchKey = req.getSearchKey();
         req.setSearchKey(null);
-        return get(QiChaChaUrl.ENTERPRISE_INFO_URL + "?searchKey=" + searchKey, req,
-                EnterpriseRes.class);
+        return get(QiChaChaUrl.ENTERPRISE_INFO_URL + "?searchKey=" + searchKey, req, EnterpriseRes.class);
     }
 
     @Override
@@ -110,5 +124,17 @@ public class QccServiceImpl implements QccService {
     @Cacheable(condition = "#result != null", cacheNames = "QI-CHA-CHA#604800", keyGenerator = "cacheKeyByParam")
     public CreditCodeRes getCreditCode(CreditCodeReq req) {
         return get(QiChaChaUrl.CREDIT_INFO_URL + "?keyWord=" + req.getKeyWord(), new QccReq(), CreditCodeRes.class);
+    }
+
+    public RestTemplate getRestTemplate() {
+        RestTemplate restTemp = restService.getNoProxyRestTemplate();
+        if (proxyEnable) {
+            HttpComponentsClientHttpRequestFactory httpRequestFactory = (HttpComponentsClientHttpRequestFactory) restTemp.getRequestFactory();
+            HttpHost proxy = new HttpHost(proxyHost, proxyPort);
+            httpRequestFactory.setHttpClient(HttpClients.custom().setProxy(proxy).build());
+            restTemp.setRequestFactory(httpRequestFactory);
+            return restTemp;
+        }
+        return restTemp;
     }
 }
