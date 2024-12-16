928fe035faecb5d3c57565697beca4e10f02ca4c
swansrp <56093273@qq.com>
2024-12-16 22:51:30 +0800

    修复使用class反射调用函数失败的问题

---
+++ kernel/src/main/java/com/bidr/kernel/utils/PrivacyUtil.java
@@ -0,0 +1,20 @@
+package com.bidr.kernel.utils;
+
+/**
+ * Title: PrivacyUtil
+ * Description: Copyright: Copyright (c) 2024 Company: Bidr Ltd.
+ *
+ * @author Sharp
+ * @since 2024/12/16 22:49
+ */
+
+public class PrivacyUtil {
+
+    public static String idCardPrivacy(String idCard) {
+        return idCard.substring(0, 6) + ("********") + idCard.substring(idCard.length() - 4);
+    }
+
+    public static String phoneNumber(String phoneNumber) {
+        return phoneNumber.substring(0, 3) + ("****") + phoneNumber.substring(phoneNumber.length() - 4);
+    }
+}
--- kernel/src/main/java/com/bidr/kernel/utils/ReflectionUtil.java
+++ kernel/src/main/java/com/bidr/kernel/utils/ReflectionUtil.java
@@ -771,7 +771,7 @@ public class ReflectionUtil {
         if (FuncUtil.isNotEmpty(classList)) {
             if (obj instanceof Class) {
                 try {
-                    return getMethod(obj, methodName, classList.toArray(new Class<?>[0])).invoke(null, paramArray);
+                    return getMethod((Class<?>)obj, methodName, classList.toArray(new Class<?>[0])).invoke(null, paramArray);
                 } catch (Exception var4) {
                     ReflectionUtils.handleReflectionException(var4);
                     throw new IllegalStateException("Should never get here");
@@ -784,7 +784,7 @@ public class ReflectionUtil {
         } else {
             if (obj instanceof Class) {
                 try {
-                    return getMethod(obj, methodName, classList.toArray(new Class<?>[0])).invoke(null);
+                    return getMethod((Class<?>)obj, methodName, classList.toArray(new Class<?>[0])).invoke(null);
                 } catch (Exception var4) {
                     ReflectionUtils.handleReflectionException(var4);
                     throw new IllegalStateException("Should never get here");
