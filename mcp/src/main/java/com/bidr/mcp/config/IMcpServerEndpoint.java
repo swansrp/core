a29a4a081af0246b1a04b8c0df48e0c7f9cbf346
swansrp <56093273@qq.com>
2025-06-06 09:49:04 +0800

    初始化mcp模块

---
+++ mcp/src/main/java/com/bidr/mcp/config/IMcpServerEndpoint.java
@@ -0,0 +1,9 @@
+package com.bidr.mcp.config;
+
+/**
+ * （通过此接口，自动收集 McpServerEndpoint 组件类）
+ *
+ * @author noear 2025/5/6 created
+ */
+public interface IMcpServerEndpoint {
+}
---
+++ mcp/src/main/java/com/bidr/mcp/config/McpServerConfig.java
@@ -0,0 +1,80 @@
+package com.bidr.mcp.config;
+
+import org.noear.solon.Solon;
+import org.noear.solon.ai.chat.tool.MethodToolProvider;
+import org.noear.solon.ai.mcp.server.McpServerEndpointProvider;
+import org.noear.solon.ai.mcp.server.annotation.McpServerEndpoint;
+import org.noear.solon.ai.mcp.server.prompt.MethodPromptProvider;
+import org.noear.solon.ai.mcp.server.resource.MethodResourceProvider;
+import org.noear.solon.web.servlet.SolonServletFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationUtils;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.List;
+
+/**
+ * Title: McpServerConfig
+ * Description: Copyright: Copyright (c) 2025 Company: Bidr Ltd.
+ *
+ * @author Sharp
+ * @since 2025/6/5 10:55
+ */
+
+@Configuration
+public class McpServerConfig {
+
+    @Value("${my.mcp.path:mcp}")
+    private String mcpPath;
+
+    @PostConstruct
+    public void start() {
+        Solon.start(McpServerConfig.class, new String[]{"--cfg=mcp-server.yml"});
+    }
+
+    @PreDestroy
+    public void stop() {
+        if (Solon.app() != null) {
+            Solon.stopBlock(false, Solon.cfg().stopDelay());
+        }
+    }
+
+    @Bean
+    public McpServerConfig init(List<IMcpServerEndpoint> serverEndpoints) {
+        //提取实现容器里 IMcpServerEndpoint 接口的 bean ，并注册为服务端点
+        for (IMcpServerEndpoint serverEndpoint : serverEndpoints) {
+            McpServerEndpoint anno = AnnotationUtils.findAnnotation(serverEndpoint.getClass(), McpServerEndpoint.class);
+
+            if (anno == null) {
+                continue;
+            }
+
+            McpServerEndpointProvider serverEndpointProvider = McpServerEndpointProvider.builder()
+                    .from(serverEndpoint.getClass(), anno).build();
+
+            serverEndpointProvider.addTool(new MethodToolProvider(serverEndpoint));
+            serverEndpointProvider.addResource(new MethodResourceProvider(serverEndpoint));
+            serverEndpointProvider.addPrompt(new MethodPromptProvider(serverEndpoint));
+
+            serverEndpointProvider.postStart();
+
+            //可以再把 serverEndpointProvider 手动转入 SpringBoot 容器
+        }
+
+        //为了能让这个 init 能正常运行
+        return this;
+    }
+
+    @Bean
+    public FilterRegistrationBean<SolonServletFilter> mcpServerFilter() {
+        FilterRegistrationBean<SolonServletFilter> filter = new FilterRegistrationBean<>();
+        filter.setName("SolonFilter");
+        filter.addUrlPatterns("/" + mcpPath + "/*");
+        filter.setFilter(new SolonServletFilter());
+        return filter;
+    }
+}
