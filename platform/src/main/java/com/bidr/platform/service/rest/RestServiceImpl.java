3459d8c63bad1f517fd8b936a1cbe205db12f75f
swansrp <56093273@qq.com>
2025-06-12 14:46:17 +0800

    param内容为空时 baseUrl函数报错

--- platform/src/main/java/com/bidr/platform/service/rest/RestServiceImpl.java
+++ platform/src/main/java/com/bidr/platform/service/rest/RestServiceImpl.java
@@ -125,51 +125,6 @@ public class RestServiceImpl implements RestService {
         return exec(restTemplate, url, method, header, param, body, collectionClass, elementClasses);
     }
 
-    @Override
-    @Retryable(value = {Exception.class}, maxAttempts = 4, backoff = @Backoff(delay = 1000L, multiplier = 2))
-    public <T> T exec(RestTemplate template, String url, HttpMethod method, HttpHeaders header, Object param,
-                      Object body, Class<?> collectionClass, Class<?>... elementClasses) {
-        ResponseEntity<T> response;
-        RestTemplate restTemp = template == null ? restTemplate : template;
-
-        ParameterizedTypeReference<T> parameter = new ParameterizedTypeReference<T>() {
-        };
-        if (param != null) {
-            url = buildUrl(url, param);
-        }
-        String transactionId = RandomUtil.randomString(7);
-        log.debug("[{}][{}] ==> {}", transactionId, method, url);
-        log.debug("[{}][header] : {}", transactionId, JsonUtil.toJson(header));
-        HttpEntity<Object> entity = new HttpEntity<>(body, header);
-        try {
-            log.debug("[{}]==> {}", transactionId, JsonUtil.toJson(body));
-            long startTime = System.currentTimeMillis();
-            response = restTemp.exchange(url, method, entity, parameter);
-            long endTime = System.currentTimeMillis();
-            String duration = String.valueOf(endTime - startTime);
-            try {
-                log.debug("[{}]({})<== {}", transactionId, duration, JsonUtil.toJson(response.getBody()));
-                return JsonUtil.readJson(response.getBody(), collectionClass, elementClasses);
-            } catch (Exception e) {
-                return response.getBody();
-            }
-        } catch (RestTemplateException e) {
-            log.error("[{}]通信异常: {}", transactionId, e.getMsg());
-            throw e;
-        }
-    }
-
-    @Override
-    public RestTemplate getNoProxyRestTemplate() {
-        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-        RestTemplate restTemp = new RestTemplate(httpRequestFactory);
-        restTemp.setRequestFactory(httpRequestFactory);
-        restTemp.setErrorHandler(restTemplate.getErrorHandler());
-        restTemp.setMessageConverters(restTemplate.getMessageConverters());
-        restTemp.setInterceptors(restTemplate.getInterceptors());
-        return restTemp;
-    }
-
     public String buildUrl(String url, Object param) {
         String paramUrl = url.contains("?") ? "&" : "?";
         Map<String, Object> paramMap = JsonUtil.readJson(param, Map.class, String.class, Object.class);
@@ -189,7 +144,12 @@ public class RestServiceImpl implements RestService {
                 e.printStackTrace();
             }
         }
-        return url + paramUrl.substring(0, paramUrl.lastIndexOf('&'));
+        int index = paramUrl.lastIndexOf('&');
+        if (index < 0) {
+            return url;
+        } else {
+            return url + paramUrl.substring(0, index);
+        }
     }
 
     @Override
@@ -296,5 +256,50 @@ public class RestServiceImpl implements RestService {
     public <T> T post(RestTemplate template, String url, HttpHeaders header, Object param, LinkedMultiValueMap body,
                       Class<?> collectionClass, Class<?>... elementClasses) {
         return getSelf().exec(template, url, HttpMethod.POST, header, param, body, collectionClass, elementClasses);
+    }
+
+    @Override
+    @Retryable(value = {Exception.class}, maxAttempts = 4, backoff = @Backoff(delay = 1000L, multiplier = 2))
+    public <T> T exec(RestTemplate template, String url, HttpMethod method, HttpHeaders header, Object param,
+                      Object body, Class<?> collectionClass, Class<?>... elementClasses) {
+        ResponseEntity<T> response;
+        RestTemplate restTemp = template == null ? restTemplate : template;
+
+        ParameterizedTypeReference<T> parameter = new ParameterizedTypeReference<T>() {
+        };
+        if (param != null) {
+            url = buildUrl(url, param);
+        }
+        String transactionId = RandomUtil.randomString(7);
+        log.debug("[{}][{}] ==> {}", transactionId, method, url);
+        log.debug("[{}][header] : {}", transactionId, JsonUtil.toJson(header));
+        HttpEntity<Object> entity = new HttpEntity<>(body, header);
+        try {
+            log.debug("[{}]==> {}", transactionId, JsonUtil.toJson(body));
+            long startTime = System.currentTimeMillis();
+            response = restTemp.exchange(url, method, entity, parameter);
+            long endTime = System.currentTimeMillis();
+            String duration = String.valueOf(endTime - startTime);
+            try {
+                log.debug("[{}]({})<== {}", transactionId, duration, JsonUtil.toJson(response.getBody()));
+                return JsonUtil.readJson(response.getBody(), collectionClass, elementClasses);
+            } catch (Exception e) {
+                return response.getBody();
+            }
+        } catch (RestTemplateException e) {
+            log.error("[{}]通信异常: {}", transactionId, e.getMsg());
+            throw e;
+        }
+    }
+
+    @Override
+    public RestTemplate getNoProxyRestTemplate() {
+        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        RestTemplate restTemp = new RestTemplate(httpRequestFactory);
+        restTemp.setRequestFactory(httpRequestFactory);
+        restTemp.setErrorHandler(restTemplate.getErrorHandler());
+        restTemp.setMessageConverters(restTemplate.getMessageConverters());
+        restTemp.setInterceptors(restTemplate.getInterceptors());
+        return restTemp;
     }
 }
