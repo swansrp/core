dbc0fd98f0573966520165dc45ac9fb6158f66f5
swansrp <56093273@qq.com>
2025-11-24 23:49:38 +0800

    提早数据库版本进化执行时间

--- kernel/src/main/java/com/bidr/kernel/config/db/MybatisPlusConfig.java
+++ kernel/src/main/java/com/bidr/kernel/config/db/MybatisPlusConfig.java
@@ -3,11 +3,12 @@ package com.bidr.kernel.config.db;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.bidr.kernel.mybatis.inf.MybatisPlusTableInitializerInf;
-import org.springframework.boot.CommandLineRunner;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.util.Map;
@@ -17,10 +18,13 @@ import java.util.Map;
  *
  * @author zong_b
  */
+@Slf4j
 @Configuration
 public class MybatisPlusConfig {
     @Resource
     private AppProperties appProperties;
+    @Resource
+    private ApplicationContext applicationContext;
     @Resource
     private DataSource dataSource;
 
@@ -42,14 +46,13 @@ public class MybatisPlusConfig {
         return new MPJConfig();
     }
 
-    @Bean
-    public CommandLineRunner initTables(ApplicationContext applicationContext) {
-        return args -> {
-            Map<String, MybatisPlusTableInitializerInf> beans = applicationContext.getBeansOfType(
-                    MybatisPlusTableInitializerInf.class);
-            for (MybatisPlusTableInitializerInf initializer : beans.values()) {
-                initializer.initTable(dataSource);
-            }
-        };
+    @PostConstruct
+    public void initTables() {
+        Map<String, MybatisPlusTableInitializerInf> beans =
+                applicationContext.getBeansOfType(MybatisPlusTableInitializerInf.class);
+
+        for (MybatisPlusTableInitializerInf initializer : beans.values()) {
+            initializer.initTable(dataSource);
+        }
     }
 }
